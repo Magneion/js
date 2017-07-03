94bb30d0be04eccb7a7676f119bac1ebbd30470f
popschool <popschool@debian>
2017-07-03 14:57:30 +0200

    function functionning, css being customized

--- js11.js
+++ js11.js
@@ -1,5 +1,5 @@
 var monAdd = function(number1, number2){
-  return number1 + number2;
+  return parseInt(number1) + parseInt(number2)
 }
 
-var box = document.getElementById("box").innerHTML = monAdd(number1, number2).value;
+// var box = document.getElementById("box").innerHTML = monAdd(number1, number2).value;
