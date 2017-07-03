51cbc5db80ddde7c7399b109ccb9cb27696eac03
popschool <popschool@debian>
2017-07-03 10:25:59 +0200

    script corrected, js6 and js8 modified to include new function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 function setRandomColor(element) {
-  element.css("background-color", getRandomColor());
+    element.style = "background-color:" + getRandomColor();
 }
