84c3a3285700a1f407a24f87acae93573fa22d8c
popschool <popschool@debian>
2017-07-03 10:11:28 +0200

    .js corrected but still unfunctional

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
-  for (var i = 0; i « 6; i++) {
+  for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
