a525cdc47dc869b1ba22241416c42b1ddb156e87
popschool <popschool@debian>
2017-07-03 12:13:53 +0200

    exercise form

--- script1.js
+++ script1.js
@@ -1,1 +1,12 @@
-var myName;
+var printForm = function() {
+  var box = documents.getElementsById("box");
+  box.innerHTML =
+  "Nom: " + document.getElementById('lastname').value + "<br />" +
+  "Prénom: " + document.getElementById('firstname').value + "<br />" +
+  "Date de naissance: " + document.getElementById('birthdate').value + "<br />" +
+  "Adresse: " + document.getElementById('address').value + "<br />" +
+  "Code postal: " + document.getElementById('zipcode').value + "<br />"
+  "Ville: " + document.getElementById('city').value + "<br />"
+  "Genre: " + document.getElementById('gender').value + "<br />"
+  "Maison: " + document.getElementById('gender').value + "<br />";
+}
